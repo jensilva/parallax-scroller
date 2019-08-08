7b170f021f6534d97e745cb220458ebb1e996c42
Jennfer Silva <jenny.ms@outlook.com.br>
2019-08-08 15:40:10 +0100

    Part 3 - Add Object Pool

--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,35 @@
 function Main() {
-    this.stage = new PIXI.Container();
-    this.renderer = PIXI.autoDetectRenderer(
-      512,
-      384,
-      {view:document.getElementById("game-canvas")}
-    );
-  
-    this.scroller = new Scroller(this.stage);
-  
-    requestAnimationFrame(this.update.bind(this));
-  }
+	this.stage = new PIXI.Container();
+	this.renderer = PIXI.autoDetectRenderer(
+		512,
+		384,
+		{view:document.getElementById("game-canvas")}
+	);
 
-  Main.SCROLL_SPEED = 3;
+	this.loadSpriteSheet();
+}
 
-  Main.prototype.update = function() {
-    this.scroller.moveViewportXBy(Main.SCROLL_SPEED);
-    this.renderer.render(this.stage);
-    requestAnimationFrame(this.update.bind(this));
-  };
+Main.SCROLL_SPEED = 3;
+
+Main.prototype.update = function() {
+	this.scroller.moveViewportXBy(Main.SCROLL_SPEED);
+	this.renderer.render(this.stage);
+	requestAnimationFrame(this.update.bind(this));
+};
+
+Main.prototype.loadSpriteSheet = function() {
+	const loader = PIXI.loader;
+	loader.add("wall", "../resources/wall.json");
+	loader.add("bg-mid", "../resources/bg-mid.png");
+	loader.add("bg-far", "../resources/bg-far.png");
+	loader.once("complete", this.spriteSheetLoaded.bind(this));
+	loader.load();
+};
+
+Main.prototype.spriteSheetLoaded = function() {
+	this.scroller = new Scroller(this.stage);
+	requestAnimationFrame(this.update.bind(this));
+
+};
+
+
